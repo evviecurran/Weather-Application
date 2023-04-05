a33dcdc081928a265f68236327950faf8b4209a3
evviecurran <125322606+evviecurran@users.noreply.github.com>
2023-04-04 18:38:42 -0600

    change

--- Assets/script.js
+++ Assets/script.js
@@ -40,40 +40,14 @@ var getCurrentConditions = (event) => {
         getFiveDayForecast(event);
       
         $('#header-text').text(response.name);
-        
-        let currentWeatherHTML = `
-            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
-            <ul class="list-unstyled">
-                <li>Temperature: ${response.main.temp}&#8457;</li>
-                <li>Humidity: ${response.main.humidity}%</li>
-                <li>Wind Speed: ${response.wind.speed} mph</li>
-                <li id="uvIndex">UV Index:</li>
-            </ul>`;
-       
-        $('#current-weather').html(currentWeatherHTML);
-       
-        let latitude = response.coord.lat;
-        let longitude = response.coord.lon;
-        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
-       
-        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+   
         
         fetch(uvQueryURL)
         .then(handleErrors)
         .then((response) => {
             return response.json();
         })
-        .then((response) => {
-            let uvIndex = response.value;
-            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
-            if (uvIndex>=0 && uvIndex<3){
-                $('#uvVal').attr("class", "uv-favorable");
-            } else if (uvIndex>=3 && uvIndex<8){
-                $('#uvVal').attr("class", "uv-moderate");
-            } else if (uvIndex>=8){
-                $('#uvVal').attr("class", "uv-severe");
-            }
-        });
+       
     })
 }
 
@@ -188,13 +162,6 @@ currentCity = $('#search-city').val();
 getCurrentConditions(event);
 });
 
-
-$('#city-results').on("click", (event) => {
-    event.preventDefault();
-    $('#search-city').val(event.target.textContent);
-    currentCity=$('#search-city').val();
-    getCurrentConditions(event);
-});
 
 
 $("#clear-storage").on("click", (event) => {
